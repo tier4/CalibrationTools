3109a642909babdb18a589dc368fabaa691d79fc
Kenzo Lobos Tsunekawa <kenzo.lobos@tier4.jp>
2023-01-31 10:39:15 +0900

    feat(intrinsic_camera_calibrator): initial release (#65)
    * Partial implementation of the new intrinsic camera calibrator
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Improved the data selection for bigger boards somewhat similar to the image pipeline
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Implemented all up until the calibration. Need to implement other sources, views, and I/O
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Added interface to load rosbags directly
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Implemented missing views and fixed some bugs (views: unrectified, rectifies, training, and evaluation)
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Removed debug code
    
    * Added the copyright notice
    Added a simple launcher with the corresponding cfg file
    Implemented save functionalities
    Added docstring documentation
    Removed some debug info
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed several bugs and added the image file source interface
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed the ruamel versin in the package xml
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Implemented the chess board detector and commented out unimplemented features
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Implemented calibration through apriltag arrays
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed some typos
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Implemented the evaluation mode and initial intrinsics loading
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Implemented a fast calibration routine that is cointinuously executed during the main calibration process.
    This is due to the fact that some of the data collection criteria use the intrinsics to obtain 3d information
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed small errors and cosmestic changes
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed some bugs and applied some suggestions from the code review
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed argument format
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Added documentation for the new intrinsic camera calibrator
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Attempted to center the images
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * New attempt
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Yet again
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Yet again
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Now the caption is not centered
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Forgot the extension change
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed the enbaled state of the calibration and evaluation buttons when no data was yet available
    Also added references to the documentation
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed more details in the documentation
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed the pause/resume functionality in sources other than topics. Implemented one change from the PR
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed a lot of typos
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed more typos
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    * Fixed tests
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
    
    ---------
    
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>

---
+++ sensor/intrinsic_camera_calibrator/intrinsic_camera_calibrator/setup.py
@@ -0,0 +1,30 @@
+from glob import glob
+import os
+
+from setuptools import setup
+
+package_name = "intrinsic_camera_calibrator"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name), glob("launch/*launch.[pxy][yma]*")),
+        ("share/" + package_name + "/config", glob("config/*")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Kenzo Lobos Tsunekawa",
+    maintainer_email="kenzo.lobos@tier4.jp",
+    description="The intrinsic camera calibrator",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "camera_calibrator = intrinsic_camera_calibrator.camera_calibrator:main"
+        ],
+    },
+)
