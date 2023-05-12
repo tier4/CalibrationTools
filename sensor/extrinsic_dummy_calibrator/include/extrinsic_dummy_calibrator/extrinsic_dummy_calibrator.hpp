3ff7dc9edba7895318ac6ddeac136a2c8dea0dba
h-ohta <hiroki.ota@tier4.jp>
2023-05-12 14:37:42 +0900

    change tf2_geometry_msgs.h to tf2_geometry_msgs.hpp

--- sensor/extrinsic_dummy_calibrator/include/extrinsic_dummy_calibrator/extrinsic_dummy_calibrator.hpp
+++ sensor/extrinsic_dummy_calibrator/include/extrinsic_dummy_calibrator/extrinsic_dummy_calibrator.hpp
@@ -36,7 +36,7 @@
 #include "pcl/filters/extract_indices.h"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 #include "tier4_calibration_msgs/srv/extrinsic_calibrator.hpp"
 
 
