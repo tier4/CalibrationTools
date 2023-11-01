96425c4437c211dd8acce83bba45cef3b4c4644e
Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
2023-11-01 16:29:03 +0900

    Implemented a ceres-based camera intrinsics calibrator. Numerical results seem to be the same as opencv but it is way faster when using a large number of calibration images
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>

---
+++ sensor/intrinsic_camera_calibrator/ceres_intrinsic_camera_calibrator/include/ceres_intrinsic_camera_calibrator/reprojection_residual.hpp
@@ -0,0 +1,163 @@
+// Copyright 2023 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CERES_INTRINSIC_CAMERA_CALIBRATOR__REPROJECTION_RESIDUAL_HPP_
+#define CERES_INTRINSIC_CAMERA_CALIBRATOR__REPROJECTION_RESIDUAL_HPP_
+
+#include <Eigen/Core>
+#include <opencv2/core.hpp>
+
+#include <ceres/autodiff_cost_function.h>
+#include <ceres/ceres.h>
+#include <ceres/rotation.h>
+
+struct ReprojectionResidual
+{
+  template <class T>
+  using Vector3 = Eigen::Matrix<T, 3, 1>;
+
+  static constexpr int POSE_OPT_DIM = 7;
+
+  static constexpr int ROTATION_W_INDEX = 0;
+  static constexpr int ROTATION_X_INDEX = 1;
+  static constexpr int ROTATION_Y_INDEX = 2;
+  static constexpr int ROTATION_Z_INDEX = 3;
+  static constexpr int TRANSLATION_X_INDEX = 4;
+  static constexpr int TRANSLATION_Y_INDEX = 5;
+  static constexpr int TRANSLATION_Z_INDEX = 6;
+
+  static constexpr int INTRINSICS_CX_INDEX = 0;
+  static constexpr int INTRINSICS_CY_INDEX = 1;
+  static constexpr int INTRINSICS_FX_INDEX = 2;
+  static constexpr int INTRINSICS_FY_INDEX = 3;
+
+  static constexpr int RESIDUAL_DIM = 2;
+
+  ReprojectionResidual(
+    const cv::Point3f & object_point, const cv::Point2f & image_point, int radial_distortion_coeffs,
+    bool use_tangential_distortion)
+  {
+    object_point_ = Eigen::Vector3d(object_point.x, object_point.y, object_point.z);
+    image_point_ = Eigen::Vector2d(image_point.x, image_point.y);
+    radial_distortion_coeffs_ = radial_distortion_coeffs;
+    use_tangential_distortion_ = use_tangential_distortion;
+  }
+
+  /*!
+   * The cost function representing the reprojection error
+   * @param[in] camera_intrinsics The camera intrinsics
+   * @param[in] board_pose The pose from ground plane to the tag (only used when using ground tags)
+   * @param[in] residuals The residual error of projecting the tag into the camera
+   * @returns success status
+   */
+  template <typename T>
+  bool operator()(
+    const T * const camera_intrinsics, const T * const board_pose, T * residuals) const
+  {
+    const Eigen::Map<const Vector3<T>> board_translation(&board_pose[TRANSLATION_X_INDEX]);
+
+    Eigen::Quaternion<T> board_quaternion = {
+      board_pose[ROTATION_W_INDEX], board_pose[ROTATION_X_INDEX], board_pose[ROTATION_Y_INDEX],
+      board_pose[ROTATION_Z_INDEX]};
+
+    board_quaternion = board_quaternion.normalized();
+
+    Vector3<T> object_point_ccs = board_quaternion * (T(1.0) * object_point_) + board_translation;
+
+    const T null_value = T(0.0);
+    const T & cx = camera_intrinsics[INTRINSICS_CX_INDEX];
+    const T & cy = camera_intrinsics[INTRINSICS_CY_INDEX];
+    const T & fx = camera_intrinsics[INTRINSICS_FX_INDEX];
+    const T & fy = camera_intrinsics[INTRINSICS_FY_INDEX];
+    const T & k1 = radial_distortion_coeffs_ > 0 ? camera_intrinsics[4] : null_value;
+    const T & k2 = radial_distortion_coeffs_ > 1 ? camera_intrinsics[5] : null_value;
+    const T & k3 = radial_distortion_coeffs_ > 2 ? camera_intrinsics[6] : null_value;
+    const T & p1 =
+      use_tangential_distortion_ ? camera_intrinsics[4 + radial_distortion_coeffs_] : null_value;
+    const T & p2 =
+      use_tangential_distortion_ ? camera_intrinsics[5 + radial_distortion_coeffs_] : null_value;
+
+    const T xp = object_point_ccs.x() / object_point_ccs.z();
+    const T yp = object_point_ccs.y() / object_point_ccs.z();
+    const T r2 = xp * xp + yp * yp;
+    const T d = 1.0 + k1 * r2 + k2 * r2 * r2 + k3 * r2 * r2 * r2;
+    const T xy = xp * yp;
+    const T tdx = 2.0 * p1 * xy + p2 * (r2 + 2.0 * xp * xp);
+    const T tdy = 2.0 * p2 * xy + p1 * (r2 + 2.0 * yp * yp);
+
+    const T predicted_ics_x = cx + fx * (xp * d + tdx);
+    const T predicted_ics_y = cy + fy * (yp * d + tdy);
+
+    residuals[0] = predicted_ics_x - image_point_.x();
+    residuals[1] = predicted_ics_y - image_point_.y();
+
+    return true;
+  }
+
+  /*!
+   * Residual factory method
+   * @param[in] object_point The object point
+   * @param[in] image_point The image point
+   * @param[in] radial_distortion_coeffs The number of radial distortion coefficients
+   * @param[in] use_tangential_distortion Whether to use or not tangenrial distortion
+   * @returns the ceres residual
+   */
+  static ceres::CostFunction * createResidual(
+    const cv::Point3f & object_point, const cv::Point2f & image_point, int radial_distortion_coeffs,
+    bool use_tangential_distortion)
+  {
+    auto f = new ReprojectionResidual(
+      object_point, image_point, radial_distortion_coeffs, use_tangential_distortion);
+
+    int distortion_coefficients =
+      radial_distortion_coeffs + 2 * static_cast<int>(use_tangential_distortion);
+    ceres::CostFunction * cost_function = nullptr;
+
+    switch (distortion_coefficients) {
+      case 0:
+        cost_function =
+          new ceres::AutoDiffCostFunction<ReprojectionResidual, RESIDUAL_DIM, 4, POSE_OPT_DIM>(f);
+        break;
+      case 1:
+        cost_function =
+          new ceres::AutoDiffCostFunction<ReprojectionResidual, RESIDUAL_DIM, 5, POSE_OPT_DIM>(f);
+        break;
+      case 2:
+        cost_function =
+          new ceres::AutoDiffCostFunction<ReprojectionResidual, RESIDUAL_DIM, 6, POSE_OPT_DIM>(f);
+        break;
+      case 3:
+        cost_function =
+          new ceres::AutoDiffCostFunction<ReprojectionResidual, RESIDUAL_DIM, 7, POSE_OPT_DIM>(f);
+        break;
+      case 4:
+        cost_function =
+          new ceres::AutoDiffCostFunction<ReprojectionResidual, RESIDUAL_DIM, 8, POSE_OPT_DIM>(f);
+        break;
+      case 5:
+        cost_function =
+          new ceres::AutoDiffCostFunction<ReprojectionResidual, RESIDUAL_DIM, 9, POSE_OPT_DIM>(f);
+        break;
+    }
+
+    return cost_function;
+  }
+
+  Eigen::Vector3d object_point_;
+  Eigen::Vector2d image_point_;
+  int radial_distortion_coeffs_;
+  bool use_tangential_distortion_;
+};
+
+#endif  // CERES_INTRINSIC_CAMERA_CALIBRATOR__REPROJECTION_RESIDUAL_HPP_
---
+++ sensor/intrinsic_camera_calibrator/ceres_intrinsic_camera_calibrator/src/ceres_camera_intrinsics_optimizer.cpp
@@ -0,0 +1,309 @@
+// Copyright 2023 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <Eigen/Core>
+#include <ceres_intrinsic_camera_calibrator/ceres_camera_intrinsics_optimizer.hpp>
+#include <ceres_intrinsic_camera_calibrator/reprojection_residual.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/core/affine.hpp>
+#include <opencv2/core/eigen.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <ceres/ceres.h>
+#include <ceres/loss_function.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include <algorithm>
+#include <numeric>
+
+void CeresCameraIntrinsicsOptimizer::setRadialDistortionCoefficients(
+  int radial_distortion_coefficients)
+{
+  radial_distortion_coefficients_ = radial_distortion_coefficients;
+}
+
+void CeresCameraIntrinsicsOptimizer::setTangentialDistortion(bool use_tangential_distortion)
+{
+  use_tangential_distortion_ = use_tangential_distortion;
+}
+
+void CeresCameraIntrinsicsOptimizer::setVerbose(bool verbose) { verbose_ = verbose; }
+
+void CeresCameraIntrinsicsOptimizer::setData(
+  const cv::Mat_<double> & camera_matrix, const cv::Mat_<double> & distortion_coeffs,
+  const std::vector<std::vector<cv::Point3f>> & object_points,
+  const std::vector<std::vector<cv::Point2f>> & image_points, const std::vector<cv::Mat> & rvecs,
+  const std::vector<cv::Mat> & tvecs)
+{
+  camera_matrix_ = camera_matrix.clone();
+  distortion_coeffs_ = distortion_coeffs.clone();
+  object_points_ = object_points;
+  image_points_ = image_points;
+  rvecs_ = rvecs;
+  tvecs_ = tvecs;
+}
+
+double CeresCameraIntrinsicsOptimizer::getSolution(
+  cv::Mat_<double> & camera_matrix, cv::Mat_<double> & distortion_coeffs,
+  std::vector<cv::Mat> & rvecs, std::vector<cv::Mat> & tvecs)
+{
+  camera_matrix = camera_matrix_;
+  distortion_coeffs = distortion_coeffs_;
+  rvecs = rvecs_;
+  tvecs = tvecs_;
+
+  double total_error = 0.0;
+  std::size_t total_points = 0;
+
+  auto get_reprojection_error = [](auto & image_points, auto & projected_points) -> double {
+    cv::Mat x = cv::Mat(2 * image_points.size(), 1, CV_32F, image_points.data());
+    cv::Mat y = cv::Mat(2 * projected_points.size(), 1, CV_32F, projected_points.data());
+    double total_error = cv::norm(x - y, cv::NORM_L2SQR);
+    return total_error;
+  };
+
+  for (std::size_t i = 0; i < object_points_.size(); i++) {
+    std::vector<cv::Point2f> projected_points;
+    total_points += object_points_[i].size();
+
+    cv::projectPoints(
+      object_points_[i], rvecs[i], tvecs[i], camera_matrix, distortion_coeffs, projected_points);
+
+    total_error += get_reprojection_error(projected_points, image_points_[i]);
+  }
+
+  double rms_error = std::sqrt(total_error / total_points);
+  return rms_error;
+}
+
+void CeresCameraIntrinsicsOptimizer::dataToPlaceholders()
+{
+  // Convert the intrinsics
+  intrinsics_placeholder_[INTRINSICS_CX_INDEX] = camera_matrix_(0, 2);
+  intrinsics_placeholder_[INTRINSICS_CY_INDEX] = camera_matrix_(1, 2);
+  intrinsics_placeholder_[INTRINSICS_FX_INDEX] = camera_matrix_(0, 0);
+  intrinsics_placeholder_[INTRINSICS_FY_INDEX] = camera_matrix_(1, 1);
+
+  double k1 = distortion_coeffs_(0);
+  double k2 = distortion_coeffs_(1);
+  double p1 = distortion_coeffs_(2);
+  double p2 = distortion_coeffs_(3);
+  double k3 = distortion_coeffs_(4);
+
+  int index = 4;
+
+  if (radial_distortion_coefficients_ > 0) {
+    intrinsics_placeholder_[index++] = k1;
+  }
+  if (radial_distortion_coefficients_ > 1) {
+    intrinsics_placeholder_[index++] = k2;
+  }
+  if (radial_distortion_coefficients_ > 2) {
+    intrinsics_placeholder_[index++] = k3;
+  }
+  if (use_tangential_distortion_) {
+    intrinsics_placeholder_[index++] = p1;
+    intrinsics_placeholder_[index++] = p2;
+  }
+
+  // Convert the revcs, tvecs into the placeholders
+  pose_placeholders_.resize(object_points_.size());
+
+  for (std::size_t i = 0; i < object_points_.size(); i++) {
+    cv::Mat rmat;
+    cv::Rodrigues(rvecs_[i], rmat);
+
+    Eigen::Vector3d translation;
+    Eigen::Matrix3d rotation;
+    cv::cv2eigen(tvecs_[i], translation);
+    cv::cv2eigen(rmat, rotation);
+    Eigen::Quaterniond quat(rotation);
+
+    std::array<double, POSE_OPT_DIM> & placeholder = pose_placeholders_[i];
+    placeholder[ROTATION_W_INDEX] = quat.w();
+    placeholder[ROTATION_X_INDEX] = quat.x();
+    placeholder[ROTATION_Y_INDEX] = quat.y();
+    placeholder[ROTATION_Z_INDEX] = quat.z();
+    placeholder[TRANSLATION_X_INDEX] = translation.x();
+    placeholder[TRANSLATION_Y_INDEX] = translation.y();
+    placeholder[TRANSLATION_Z_INDEX] = translation.z();
+  }
+}
+
+void CeresCameraIntrinsicsOptimizer::placeholdersToData()
+{
+  // Convert the intrinsics
+  camera_matrix_(0, 2) = intrinsics_placeholder_[INTRINSICS_CX_INDEX];
+  camera_matrix_(1, 2) = intrinsics_placeholder_[INTRINSICS_CY_INDEX];
+  camera_matrix_(0, 0) = intrinsics_placeholder_[INTRINSICS_FX_INDEX];
+  camera_matrix_(1, 1) = intrinsics_placeholder_[INTRINSICS_FY_INDEX];
+
+  distortion_coeffs_ = cv::Mat_<double>::zeros(5, 1);
+  double & k1 = distortion_coeffs_(0);
+  double & k2 = distortion_coeffs_(1);
+  double & p1 = distortion_coeffs_(2);
+  double & p2 = distortion_coeffs_(3);
+  double & k3 = distortion_coeffs_(4);
+
+  int index = 4;
+
+  if (radial_distortion_coefficients_ > 0) {
+    k1 = intrinsics_placeholder_[index++];
+  }
+  if (radial_distortion_coefficients_ > 1) {
+    k2 = intrinsics_placeholder_[index++];
+  }
+  if (radial_distortion_coefficients_ > 2) {
+    k3 = intrinsics_placeholder_[index++];
+  }
+  if (use_tangential_distortion_) {
+    p1 = intrinsics_placeholder_[index++];
+    p2 = intrinsics_placeholder_[index++];
+  }
+
+  // Convert the revcs, tvecs into the placeholders
+  rvecs_.resize(pose_placeholders_.size());
+  tvecs_.resize(pose_placeholders_.size());
+
+  for (std::size_t i = 0; i < pose_placeholders_.size(); i++) {
+    const auto & placeholder = pose_placeholders_[i];
+    const double scale = 1.0 / std::sqrt(
+                                 placeholder[0] * placeholder[0] + placeholder[1] * placeholder[1] +
+                                 placeholder[2] * placeholder[2] + placeholder[3] * placeholder[3]);
+
+    Eigen::Quaterniond quat = Eigen::Quaterniond(
+      scale * placeholder[ROTATION_W_INDEX], scale * placeholder[ROTATION_X_INDEX],
+      scale * placeholder[ROTATION_Y_INDEX], scale * placeholder[ROTATION_Z_INDEX]);
+
+    Eigen::Vector3d translation = Eigen::Vector3d(
+      placeholder[TRANSLATION_X_INDEX], placeholder[TRANSLATION_Y_INDEX],
+      placeholder[TRANSLATION_Z_INDEX]);
+
+    Eigen::Matrix3d rotation = quat.toRotationMatrix();
+
+    cv::Matx33d cv_rot;
+    cv::Matx31d cv_transl;
+    cv::eigen2cv(translation, cv_transl);
+    cv::eigen2cv(rotation, cv_rot);
+
+    cv::Mat rvec, tvec;
+    tvec = cv::Mat(cv_transl);
+
+    cv::Rodrigues(cv_rot, rvec);
+
+    rvecs_[i] = rvec;
+    tvecs_[i] = tvec;
+  }
+}
+
+void CeresCameraIntrinsicsOptimizer::evaluate()
+{
+  // Start developing the ceres optimizer
+  double total_calibration_error = 0.0;
+  auto get_reprojection_error =
+    [](const auto & image_points, const auto & projected_points) -> double {
+    double total_error = 0;
+    for (size_t i = 0; i < projected_points.size(); i++) {
+      double error = cv::norm(image_points[i] - projected_points[i]);
+      total_error += error * error;
+    }
+    return std::sqrt(total_error / projected_points.size());
+  };
+
+  for (std::size_t i = 0; i < object_points_.size(); i++) {
+    std::vector<cv::Point2f> projected_points;
+    cv::projectPoints(
+      object_points_[i], rvecs_[i], tvecs_[i], camera_matrix_, distortion_coeffs_,
+      projected_points);
+
+    double calibration_error = get_reprojection_error(projected_points, image_points_[i]);
+
+    total_calibration_error += calibration_error;
+  }
+
+  if (verbose_) {
+    printf("summary | calibration_error=%.3f\n", total_calibration_error / object_points_.size());
+  }
+
+  double total_ceres_error = 0;
+
+  for (std::size_t view_index = 0; view_index < object_points_.size(); view_index++) {
+    const auto & view_object_points = object_points_[view_index];
+    const auto & view_image_points = image_points_[view_index];
+    auto & pose_placeholder = pose_placeholders_[view_index];
+
+    for (std::size_t point_index = 0; point_index < view_object_points.size(); point_index++) {
+      auto f = ReprojectionResidual(
+        view_object_points[point_index], view_image_points[point_index],
+        radial_distortion_coefficients_, use_tangential_distortion_);
+      std::array<double, 2> residuals;
+      f(intrinsics_placeholder_.data(), pose_placeholder.data(), residuals.data());
+      total_ceres_error += residuals[0] * residuals[0] + residuals[1] * residuals[1];
+    }
+  }
+
+  if (verbose_) {
+    std::cout << "total_ceres_error: " << 0.5 * total_ceres_error << std::endl;
+  }
+}
+
+void CeresCameraIntrinsicsOptimizer::solve()
+{
+  ceres::Problem problem;
+
+  for (std::size_t view_index = 0; view_index < object_points_.size(); view_index++) {
+    const auto & view_object_points = object_points_[view_index];
+    const auto & view_image_points = image_points_[view_index];
+    auto & pose_placeholder = pose_placeholders_[view_index];
+
+    for (std::size_t point_index = 0; point_index < view_object_points.size(); point_index++) {
+      problem.AddResidualBlock(
+        ReprojectionResidual::createResidual(
+          view_object_points[point_index], view_image_points[point_index],
+          radial_distortion_coefficients_, use_tangential_distortion_),
+        nullptr,  // L2
+        intrinsics_placeholder_.data(), pose_placeholder.data());
+    }
+  }
+
+  double initial_cost = 0.0;
+  std::vector<double> residuals;
+  ceres::Problem::EvaluateOptions eval_opt;
+  eval_opt.num_threads = 1;
+  problem.GetResidualBlocks(&eval_opt.residual_blocks);
+  problem.Evaluate(eval_opt, &initial_cost, &residuals, nullptr, nullptr);
+
+  if (verbose_) {
+    std::cout << "Initial cost: " << initial_cost;
+  }
+
+  ceres::Solver::Options options;
+  options.linear_solver_type = ceres::DENSE_SCHUR;
+  options.minimizer_progress_to_stdout = verbose_;
+  options.max_num_iterations = 500;
+  options.function_tolerance = 1e-10;
+  options.gradient_tolerance = 1e-14;
+  options.num_threads = 8;
+  options.use_inner_iterations = true;
+  ceres::Solver::Summary summary;
+  ceres::Solve(options, &problem, &summary);
+
+  if (verbose_) {
+    std::cout << "Report: " << summary.FullReport();
+  }
+}
