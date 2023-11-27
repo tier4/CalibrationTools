ad628cce9efa3d8597bd6624d1312c9b0547557b
vividf <yihsiang.fang@tier4.jp>
2023-11-27 13:40:04 +0900

    clean code

--- sensor/extrinsic_reflector_based_calibrator/scripts/metrics_plotter_node.py
+++ sensor/extrinsic_reflector_based_calibrator/scripts/metrics_plotter_node.py
@@ -19,7 +19,6 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
-import matplotlib
 import matplotlib.pyplot as plt
 
 class Plotter:
@@ -55,16 +54,13 @@ class Plotter:
         self.axes[1, 1].set_xlabel('# reflector')
         self.axes[1, 1].set_ylabel('Yaw error')
 
+
     def remove_anno(self, anno):
         if(anno is not None):
             anno.remove()
 
+
     def draw_graph(self, msg):
-        # Make the plot dynamic
-        #if(x > repeat_length):
-        #    plt.xlim(x - repeat_length, x)
-        #else:
-        #    plt.xlim(0, repeat_length)
         num_of_reflectors = msg.data[0]
         cv_distance_error = msg.data[1]
         cv_yaw_error = msg.data[2]
@@ -103,7 +99,6 @@ class Plotter:
         self.anno1 = self.axes[0, 1].annotate('%0.4f' % cv_yaw_error, xy=(num_of_reflectors, cv_yaw_error), color='g')
         self.anno2 = self.axes[1, 0].annotate('%0.4f' % calibration_distance_error, xy=(num_of_reflectors, calibration_distance_error), color='b')
         self.anno3 = self.axes[1, 1].annotate('%0.4f' % calibration_yaw_error, xy=(num_of_reflectors, calibration_yaw_error), color='g')
-        #plt.ion()
         plt.pause(0.1)
 
         self.prev_cv_distance_error = cv_distance_error
@@ -122,24 +117,15 @@ class MetricsPlotter(Node):
             'cross_validation_metrics',
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
-
 
     def listener_callback(self, msg):
         self.plotter.draw_graph(msg)
 
 
-
 def main(args=None):
     rclpy.init(args=args)
-
     metrics_plotter = MetricsPlotter()
-
     rclpy.spin(metrics_plotter)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     metrics_plotter.destroy_node()
     rclpy.shutdown()
 
