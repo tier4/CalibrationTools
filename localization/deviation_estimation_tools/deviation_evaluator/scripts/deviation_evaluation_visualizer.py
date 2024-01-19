838e882724388b4b6c413c7611112aa1d762fdbc
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2024-01-19 14:09:44 +0900

    Added cspell:words lowerbound
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- localization/deviation_estimation_tools/deviation_evaluator/scripts/deviation_evaluation_visualizer.py
+++ localization/deviation_estimation_tools/deviation_evaluator/scripts/deviation_evaluation_visualizer.py
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# cspell:words lowerbound
+
 from pathlib import Path
 from threading import Thread
 
