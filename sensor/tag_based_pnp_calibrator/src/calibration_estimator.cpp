6d9c30a22b0aadf835f971eb357453e6f4faf691
Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
2024-01-19 13:17:44 +0900

    chore: applied the fix to another file
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>

--- sensor/tag_based_pnp_calibrator/src/calibration_estimator.cpp
+++ sensor/tag_based_pnp_calibrator/src/calibration_estimator.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <Eigen/Dense>  // note: this header must come before <opencv2/core/eigen.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/core/eigen.hpp>
 #include <opencv2/imgproc.hpp>
