ca14dce9fc9efd40e5021d428000396cb1382644
Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>
2024-02-26 20:29:25 +0900

    chore: forgot to ass the mapping base-lidar for the default project
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>

--- sensor/sensor_calibration_manager/sensor_calibration_manager/calibrators/default_project/__init__.py
+++ sensor/sensor_calibration_manager/sensor_calibration_manager/calibrators/default_project/__init__.py
@@ -1,5 +1,6 @@
 from .ground_plane_calibrator import GroundPlaneCalibrator
 from .lidar_lidar_2d_calibrator import LidarLidar2DCalibrator
+from .mapping_based_base_lidar_calibrator import MappingBasedBaseLidarCalibrator
 from .mapping_based_lidar_lidar_calibrator import MappingBasedLidarLidarCalibrator
 from .tag_based_pnp_calibrator import TagBasedPNPCalibrator
 from .tag_based_sfm_base_lidar_calibrator import TagBasedSfmBaseLidarCalibrator
@@ -9,6 +10,7 @@ from .tag_based_sfm_base_lidars_cameras_calibrator import TagBasedSfmBaseLidarsC
 __all__ = [
     "GroundPlaneCalibrator",
     "LidarLidar2DCalibrator",
+    "MappingBasedBaseLidarCalibrator",
     "MappingBasedLidarLidarCalibrator",
     "TagBasedPNPCalibrator",
     "TagBasedSfmBaseLidarCalibrator",
