802f157c8aead6935e5b60066f7d81c652a9bd56
Tomoya Muraki <tomoya.muraki@tier4.jp>
2024-10-21 12:48:13 +0900

    register calibrators

---
+++ sensor_calibration_manager/sensor_calibration_manager/calibrators/x2_gen2/__init__.py
@@ -0,0 +1,13 @@
+from .tag_based_sfm_base_lidar_calibrator import TagBasedSfmBaseLidarCalibrator
+from .ground_plane_calibrator import GroundPlaneCalibrator
+from .mapping_based_lidar_lidar_calibrator import MappingBasedLidarLidarCalibrator
+from .marker_radar_lidar_calibrator import MarkerRadarLidarCalibrator
+from .tag_based_pnp_calibrator import TagBasedPNPCalibrator
+
+__all__ = [
+    "TagBasedSfmBaseLidarCalibrator",
+    "GroundPlaneCalibrator",
+    "MappingBasedLidarLidarCalibrator",
+    "MarkerRadarLidarCalibrator",
+    "TagBasedPNPCalibrator",
+]
